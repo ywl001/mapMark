47402659a0bde51cb33baa6a90e557f3265ad107
ywl01 <ywl01@163.com>
2025-04-20 09:14:58 +0800

    ok

--- miniprogram/components/map/map.ts
+++ miniprogram/components/map/map.ts
@@ -1,10 +1,8 @@
-import { MapExtent, MapRegionChangeEvent, Marker } from "../../app-types";
+import { MapRegionChangeEvent } from "../../app-types";
 import db from "../../utils/db";
 import { AppEvent } from "../../utils/event-bus/event-type";
 import EventBus from "../../utils/event-bus/EventBus";
 import markerService from "./marker-service";
-
-
 
 // components/map/map.ts
 Component({
@@ -13,16 +11,8 @@ Component({
     pureDataPattern: /^_/ // 所有 _ 开头的字段被视为纯数据字段
   },
 
-  /**
-   * 组件的属性列表
-   */
-  properties: {
+  properties: {},
 
-  },
-
-  /**
-   * 组件的初始数据
-   */
   data: {
     isLongpress: true,
     longitude: 112.461265,
@@ -35,156 +25,128 @@ Component({
   },
 
   ready() {
-    console.log('map component ready');
+    const mapContext = wx.createMapContext('map', this);
 
-    this.setData({
-      _mapContext: wx.createMapContext('map', this),
-    })
+    this.setData({ _mapContext: mapContext });
+    markerService.setMapContext(mapContext);
 
-    markerService.setMapContext(wx.createMapContext('map', this));
-
-    //从本地取出存储的上次mapExtent
-    const ex = wx.getStorageSync('mapExtent')
+    const ex = wx.getStorageSync('mapExtent');
     if (ex) {
       this.setData({
         scale: ex.scale,
         longitude: ex.longitude,
         latitude: ex.latitude
-      })
+      });
     }
 
-    EventBus.on(AppEvent.MOVE_MARKER, (e) => {
-      this.moveMarker(e)
-    })
-   
-
-
+    EventBus.on(AppEvent.MOVE_MARKER, (e) => this.moveMarker(e));
   },
 
-  /**
-   * 组件的方法列表
-   */
   methods: {
+
+    /** 地图区域变化时触发 */
     onRegionChange(e: MapRegionChangeEvent) {
-      console.log('地图区域变化：', e.type, e.detail);
-      //保存当前地图范围，下次启动从本地获取
-      // 获取地图的上下左右边界（extent）
-      if (e.type == 'end') {
+      if (e.type === 'end') {
         wx.setStorageSync('mapExtent', {
           latitude: e.detail.centerLocation.latitude,
           longitude: e.detail.centerLocation.longitude,
           scale: e.detail.scale
-        })
-        markerService.refreshMarkers(e.detail.region)
+        });
+
+        markerService.refreshMarkers(e.detail.region);
       }
     },
 
-
-    /**长按的监听 */
-    onLongpressMap(e: { detail: { x: any; y: any; }; }) {
-      //判断是否是长按
+    /** 长按地图触发添加 marker */
+    onLongpressMap(e: { detail: { x: number; y: number } }) {
       if (!this.data.isLongpress) return;
-      //长按时给的是屏幕坐标
-      const { x, y } = e.detail
 
-      if (this.data._currentScale < 18) {
-        this.screenToLocation(x, y, this.data._mapContext).then((res: any) => {
+      const { x, y } = e.detail;
+
+      const handleLocation = (res: any) => {
+        if (this.data._currentScale < 18) {
           this.setData({
             scale: 18,
             latitude: res.latitude,
             longitude: res.longitude
-          })
-        })
-      } else {
-        this.screenToLocation(x, y, this.data._mapContext).then((res: any) => {
+          });
+        } else {
           const app = getApp();
-          app.globalData.markerData = {
-            x: res.latitude,
-            y: res.longitude
-          };
+          app.globalData.markerData = { x: res.latitude, y: res.longitude };
+
           const type: string = getApp().type;
           wx.navigateTo({
-            url: type === 'policeStation' ? '/pages/create_jws/create_jws' : '/pages/camera-form/camera-form'
+            url: type === 'policeStation'
+              ? '/pages/create_jws/create_jws'
+              : '/pages/camera-form/camera-form'
           });
-        })
-      }
-    },
-
-    /**对touchStart的监听，处理当缩放手势时触发longpress事件 */
-    onMapTouchStart(e: { touches: string | any[]; }) {
-      // console.log(e)
-      this.data.isLongpress = true;
-      if (e.touches.length > 1) {
-        this.data.isLongpress = false;
-      }
-    },
-
-    onTapMap(e: { detail: { latitude: any; longitude: any; }; }) {
-      console.log('map click', e)
-      if (this.data.isMoveMarker) {
-        console.log("move marker")
-        this.setData({
-          isMoveMarker: false
-        })
-        const lat = e.detail.latitude;
-        const lng = e.detail.longitude;
-        const data = {
-          x: lat,
-          y: lng
         }
-        db.updateById('mark', this.data.moveMarkerId, data).then(() => {
-          // EventBus.emit(AppEvent.REFRESH_MARKER)
-          EventBus.emit(AppEvent.DEL_MARK, this.data.moveMarkerId)
-        })
-      } else {
-        //取消高亮marker
-        console.log('click map blank')
+      };
 
+      this.screenToLocation(x, y, this.data._mapContext).then(handleLocation);
+    },
+
+    /** 触摸地图时判断是否是缩放操作（双指）来取消长按添加 */
+    onMapTouchStart(e: { touches: any[] }) {
+      this.data.isLongpress = e.touches.length <= 1;
+    },
+
+    /** 点击地图事件 */
+    onTapMap(e: { detail: { latitude: number; longitude: number } }) {
+      if (this.data.isMoveMarker) {
+        this.setData({ isMoveMarker: false });
+
+        const { latitude: lat, longitude: lng } = e.detail;
+        const data = { x: lat, y: lng };
+
+        db.updateById('mark', this.data.moveMarkerId, data).then(() => {
+          EventBus.emit(AppEvent.DEL_MARK, this.data.moveMarkerId);
+        });
+      } else {
         EventBus.emit(AppEvent.REMOVE_MARKER_INFO);
       }
     },
 
+    /** 点击 marker 事件 */
     onMarkerTap(e: { markerId: number }) {
-      markerService.tapMarker(e.markerId)
+      markerService.tapMarker(e.markerId);
     },
 
+    /** 获取当前位置并移动地图中心 */
     getLocation() {
       wx.getLocation({}).then(res => {
-        console.log(res)
         this.data._mapContext.moveToLocation({
           latitude: res.latitude,
           longitude: res.longitude,
           fail: () => {
-            wx.showToast({
-              title: '定位失败',
-            })
+            wx.showToast({ title: '定位失败' });
           }
-        })
-      })
+        });
+      });
     },
 
+    /** 标记为待移动 marker 状态 */
     moveMarker(e: string) {
-      console.log('move marker')
       this.setData({
         isMoveMarker: true,
         moveMarkerId: e
-      })
+      });
     },
 
-    screenToLocation(x: any, y: any, mapContext: { fromScreenLocation?: any; }) {
+    /** 将屏幕坐标转换为地图经纬度 */
+    screenToLocation(x: number, y: number, mapContext: WechatMiniprogram.MapContext) {
       return new Promise((resolve, reject) => {
         mapContext.fromScreenLocation({
-          x: x, // 长按事件中的屏幕坐标 x
-          y: y, // 长按事件中的屏幕坐标 y
-          success: (res: unknown) => {
-            resolve(res)
-          },
-          fail: (err: any) => {
-            console.log('获取经纬度失败', err);
-            reject(err)
+          x,
+          y,
+          success: (res) => resolve(res),
+          fail: (err) => {
+            // 获取经纬度失败
+            reject(err);
           }
-        })
-      })
-    },
+        });
+      });
+    }
+
   }
-})
+});
--- miniprogram/components/map/marker-service.ts
+++ miniprogram/components/map/marker-service.ts
@@ -5,88 +5,99 @@ import cloudFunctions from "../../utils/cloud-functions";
 import { AppEvent } from "../../utils/event-bus/event-type";
 import EventBus from "../../utils/event-bus/EventBus";
 
-
+/**
+ * 地图 marker 管理服务类
+ */
 export class MarkersService {
-
   private markersMap: Map<string, Marker> = new Map(); // 所有 marker（key: _id）
-
-  /**和显示在地图上的marker同步，id是marker.id */
   private currentMarkersMap: Map<number, Marker> = new Map(); // 当前地图上的 marker（key: id）
-
-
   private searchMarkersMap: Map<number, Marker> = new Map(); // 搜索产生的 marker
   private markIdCount = 1;
   private userid = getApp().globalData.userid;
-
   private mapContext!: WechatMiniprogram.MapContext;
-
   private extent!: MapExtent;
-
-  private highLighMarkerId = 0
+  private highLighMarkerId = 0;
 
   constructor() {
-    EventBus.on(AppEvent.REFRESH_MARKER, () => {
-      this.refreshMarkers()
-    })
-
-    EventBus.on(AppEvent.SEARCH_MARKER, (e) => {
-      this.showSearchMarkers(e)
-    })
-
-    EventBus.on(AppEvent.CLEAR_SEARCH_MARKER, () => {
-      this.clearSearchMarker()
-    })
-
-    /**当修改、移动、删除marker时，将map中的数据清除，从服务器重新获取后存入 */
-    EventBus.on(AppEvent.DEL_MARK, (id:string) => {
-      this.markersMap.delete(id)
-      //从总map中删除
-      const mid = this.markersMap.get(id)?.id
-      if(mid){
-        //从对照地图的map中删除
-        this.mapContext.removeMarkers({markerIds:[mid]})
-        //从地图中删除
-        this.currentMarkersMap.delete(mid)
-      }
-      //刷新marker
-      this.refreshMarkers();
-
-      EventBus.emit(AppEvent.REMOVE_MARKER_INFO);
-      wx.showToast({
-        title: '成功',
-        icon: 'success',
-      });
-    })
+    EventBus.on(AppEvent.REFRESH_MARKER, () => this.refreshMarkers());
+    EventBus.on(AppEvent.SEARCH_MARKER, e => this.showSearchMarkers(e));
+    EventBus.on(AppEvent.CLEAR_SEARCH_MARKER, () => this.clearSearchMarker());
+    EventBus.on(AppEvent.DEL_MARK, (id: string) => this.deleteMarker(id));
   }
 
+  /** 设置地图上下文 */
   setMapContext(mapContext: WechatMiniprogram.MapContext) {
     this.mapContext = mapContext;
   }
 
   /** 刷新地图上的 marker，完整流程 */
   async refreshMarkers(region?: MapRegion) {
-    // 第一步：从数据库获取 extent 内新数据并存入 markersMap
-    if (region) {
-      this.extent = this.regionToExtent(region);
-    }
+    if (region) this.extent = this.regionToExtent(region);
     await this.fetchMarkers();
-
-    // 第二步：从 markersMap 中筛选当前范围内的 marker
     const markersInExtent = this.getMarkersInExtent(this.extent);
-
-    //移除范围外的marker
     this.removeMarksOutOfExtent();
-
-    //加入范围内的新增marker
-    this.addNewMarkersInExtent(markersInExtent)
+    this.addNewMarkersInExtent(markersInExtent);
   }
 
-  async fetchMarkers() {
-    const existingIds = Object.keys(this.markersMap);
+  /** 获取当前视野范围内的 marker */
+  getMarkersInExtent(extent: MapExtent): Marker[] {
+    const markers: Marker[] = [];
+    this.markersMap.forEach(marker => {
+      if (
+        marker.latitude >= extent.xmin && marker.latitude <= extent.xmax &&
+        marker.longitude >= extent.ymin && marker.longitude <= extent.ymax
+      ) {
+        markers.push(marker);
+      }
+    });
+    return markers;
+  }
+
+  /** 点击 marker 事件处理 */
+  tapMarker(markId: number) {
+    this.highlightMarker(markId);
+    const marker = this.currentMarkersMap.get(markId);
+    if (marker) EventBus.emit(AppEvent.SHOW_MARKER_INFO, marker);
+  }
+
+  /** 高亮 marker */
+  private highlightMarker(id: number) {
+    if (this.highLighMarkerId) {
+      const oldMarker = this.currentMarkersMap.get(this.highLighMarkerId);
+      if (oldMarker) {
+        oldMarker.iconPath = oldMarker.icon;
+        this.mapContext.removeMarkers({ markerIds: [oldMarker.id] });
+        this.mapContext.addMarkers({ markers: [oldMarker] });
+      }
+    }
+
+    this.highLighMarkerId = id;
+    const newMarker = this.currentMarkersMap.get(id);
+    if (newMarker) {
+      newMarker.iconPath = newMarker.iconSelected as string;
+      this.mapContext.removeMarkers({ markerIds: [newMarker.id] });
+      this.mapContext.addMarkers({ markers: [newMarker] });
+      this.mapContext.moveToLocation({ latitude: newMarker.latitude, longitude: newMarker.longitude });
+    }
+  }
+
+  /** 根据 region 转换为 extent */
+  private regionToExtent(data: MapRegion): MapExtent {
+    return {
+      xmax: data.northeast.latitude,
+      ymax: data.northeast.longitude,
+      xmin: data.southwest.latitude,
+      ymin: data.southwest.longitude,
+    };
+  }
+
+  /** 获取云端数据，并更新 markersMap */
+  private async fetchMarkers() {
+    const existingIds = Array.from(this.markersMap.keys());
     const query = {
       x: { $gte: this.extent.xmin, $lte: this.extent.xmax },
       y: { $gte: this.extent.ymin, $lte: this.extent.ymax },
-      _id: { $nin: existingIds }
+      _id: { $nin: existingIds },
     };
 
     const res: any[] = await cloudFunctions.getData('mark', query) as any[];
@@ -94,162 +105,79 @@ export class MarkersService {
       .map((data: MarkerData) => this.convertToMarker(data))
       .filter((m): m is Marker => m !== null);
 
-    markers.forEach((marker) => {
-      this.markersMap.set(marker._id, marker)
-    });
+    markers.forEach(marker => this.markersMap.set(marker._id, marker));
   }
 
-  getMarkersInExtent(extent: MapExtent): Marker[] {
-    const markers: Marker[] = [];
-
-    this.markersMap.forEach(marker => {
-      if (
-        marker.latitude >= extent.xmin &&
-        marker.latitude <= extent.xmax &&
-        marker.longitude >= extent.ymin &&
-        marker.longitude <= extent.ymax
-      ) {
-        // if (marker.id === this.highLighMarkerId) {
-        //   marker.zIndex = 9;
-        //   marker.iconPath = marker.iconSelected!;
-        // } else {
-        //   marker.iconPath = marker.icon;
-        // }
-        markers.push(marker);
-      }
-    });
-
-    return markers;
-  }
-
-  /**删除范围外的marker，同步currentMarkerMap */
+  /** 移除超出 extent 范围的 marker */
   private removeMarksOutOfExtent() {
-    let ids: number[] = []
-    this.currentMarkersMap.forEach((marker, id) => {
+    const ids: number[] = [];
+    this.currentMarkersMap.forEach(marker => {
       const inExtent =
-        marker.latitude >= this.extent.xmin &&
-        marker.latitude <= this.extent.xmax &&
-        marker.longitude >= this.extent.ymin &&
-        marker.longitude <= this.extent.ymax;
-      if (!inExtent) {
-        ids.push(marker.id);
-      }
+        marker.latitude >= this.extent.xmin && marker.latitude <= this.extent.xmax &&
+        marker.longitude >= this.extent.ymin && marker.longitude <= this.extent.ymax;
+      if (!inExtent) ids.push(marker.id);
     });
-
     ids.forEach(id => this.currentMarkersMap.delete(id));
-    this.mapContext.removeMarkers({ markerIds: ids })
+    this.mapContext.removeMarkers({ markerIds: ids });
   }
 
+  /** 添加新进入 extent 范围内的 marker */
   private addNewMarkersInExtent(markersInExtent: Marker[]) {
-
     const currentIds = new Set(this.currentMarkersMap.keys());
-
-    // 添加新 marker（在 extent 内但未添加到地图）
-    let newMarkers = []
-    for (const marker of markersInExtent) {
-      if (!currentIds.has(marker.id)) {
-        newMarkers.push(marker);
-      }
-    }
+    const newMarkers = markersInExtent.filter(marker => !currentIds.has(marker.id));
     newMarkers.forEach(marker => this.currentMarkersMap.set(marker.id, marker));
     this.mapContext.addMarkers({ markers: newMarkers });
   }
 
-
-  tapMarker(markId: number) {
-    this.highlightMarker(markId)
-    const marker = this.currentMarkersMap.get(markId)
-    console.log(marker)
-    if (marker)
-      EventBus.emit(AppEvent.SHOW_MARKER_INFO, marker)
+  /** 删除 marker 并刷新地图 */
+  private deleteMarker(id: string) {
+    this.markersMap.delete(id);
+    const mid = this.markersMap.get(id)?.id;
+    if (mid) {
+      this.mapContext.removeMarkers({ markerIds: [mid] });
+      this.currentMarkersMap.delete(mid);
+    }
+    this.refreshMarkers();
+    EventBus.emit(AppEvent.REMOVE_MARKER_INFO);
+    wx.showToast({ title: '成功', icon: 'success' });
   }
 
-  private highlightMarker(id: number) {
-    // 如果之前有高亮的 marker，先删除它
-    if (this.highLighMarkerId) {
-      console.log('remove old highlight', this.highLighMarkerId)
-      const oldMarker = this.currentMarkersMap.get(this.highLighMarkerId);
-      if (oldMarker) {
-        // 更新图标等信息
-        oldMarker.iconPath = oldMarker.icon; // 恢复原来的图标
-        this.mapContext.removeMarkers({ markerIds: [oldMarker.id] }); // 从地图中删除旧的高亮 marker
-        this.mapContext.addMarkers({ markers: [oldMarker] }); // 从地图中删除旧的高亮 marker
-        console.log('update old hightlight marker')
-      }
-    }
-
-    // 更新高亮 marker 的 id
-    this.highLighMarkerId = id;
-
-    // 获取新的高亮 marker
-    const newMarker = this.currentMarkersMap.get(id);
-    if (newMarker) {
-      // 更改高亮 marker 的图标
-      newMarker.iconPath = newMarker.iconSelected as string; // 使用选中的图标
-
-      // 将新的高亮 marker 加入地图
-      console.log('update new highlight marker')
-      this.mapContext.removeMarkers({ markerIds: [newMarker.id] });
-      this.mapContext.addMarkers({ markers: [newMarker] });
-      this.mapContext.moveToLocation({ latitude: newMarker.latitude, longitude: newMarker.longitude })
-    }
-
-  }
-
+  /** 展示搜索结果 marker */
   showSearchMarkers(markDatas: MarkerData[]) {
-    if (markDatas.length == 1) {
-      const m = markDatas[0]
+    if (markDatas.length === 1) {
+      const m = markDatas[0];
       const marker = this.convertToLocationMarker(m);
       this.searchMarkersMap.set(marker.id, marker);
-      this.mapContext.addMarkers({ markers: [marker] })
-      this.mapContext.moveToLocation({ latitude: m.x, longitude: m.y })
+      this.mapContext.addMarkers({ markers: [marker] });
+      this.mapContext.moveToLocation({ latitude: m.x, longitude: m.y });
     } else {
-      const points = markDatas.map(m => ({ latitude: m.x, longitude: m.y }))
-      const markers = markDatas.map(m => this.convertToLocationMarker(m))
-      markers.forEach(m => {
-        console.log(m.id)
-        this.searchMarkersMap.set(m.id, m)
-      })
-      this.mapContext.addMarkers({ markers })
-      this.mapContext.includePoints({ points })
+      const markers = markDatas.map(m => this.convertToLocationMarker(m));
+      const points = markDatas.map(m => ({ latitude: m.x, longitude: m.y }));
+      markers.forEach(m => this.searchMarkersMap.set(m.id, m));
+      this.mapContext.addMarkers({ markers });
+      this.mapContext.includePoints({ points });
     }
   }
 
+  /** 清除搜索产生的 marker */
   clearSearchMarker() {
-    this.mapContext.removeMarkers({ markerIds: Array.from(this.searchMarkersMap.keys()) })
+    const ids = Array.from(this.searchMarkersMap.keys());
+    this.mapContext.removeMarkers({ markerIds: ids });
+    this.searchMarkersMap.clear();
   }
 
-  convertToLocationMarker(data: MarkerData): Marker {
-    return {
-      id: this.markIdCount++,
-      _id: data._id,
-      latitude: data.x, // 将 x 转换为纬度
-      longitude: data.y, // 将 y 转换为经度
-      anchor: {
-        x: 0.5,
-        y: 1
-      },
-      iconPath: "../../assets/location.png", // 自定义图标
-      width: 40, // 图标宽度
-      height: 40, // 图标高度
-      source: data
-    };
-  }
-
+  /** MarkerData 转 Marker，自动根据类型转换 */
   private convertToMarker(data: MarkerData): Marker | null {
-    if (data.type === 'policeStation') {
-      return this.convertToJwsMarker(data);
-    } else if (data.type === 'camera') {
-      return this.convertToCameraMarker(data);
-    }
+    if (data.type === 'policeStation') return this.convertToJwsMarker(data);
+    if (data.type === 'camera') return this.convertToCameraMarker(data);
     return null;
   }
 
+  /** 转换为警务室 marker */
   private convertToJwsMarker(data: MarkerData): Marker {
     const isUser = data.userid === this.userid;
     const icon = isUser ? '../../assets/jws_user.png' : '../../assets/jws.png';
     const iconSelected = isUser ? '../../assets/jws_user2.png' : '../../assets/jws2.png';
-
     return {
       id: this.markIdCount++,
       _id: data._id,
@@ -263,17 +191,18 @@ export class MarkersService {
         anchorX: 10,
         anchorY: -10,
         borderRadius: 10,
-        textAlign: "right"
+        textAlign: "right",
       },
       iconPath: icon,
       width: 20,
       height: 20,
       source: data,
       iconSelected,
-      icon
+      icon,
     };
   }
 
+  /** 转换为摄像头 marker */
   private convertToCameraMarker(data: MarkerData): Marker {
     let icon = '', iconSelected = '';
     switch (data.cameraType) {
@@ -294,7 +223,6 @@ export class MarkersService {
         iconSelected = '../../assets/shehui_press.png';
         break;
     }
-
     return {
       id: this.markIdCount++,
       _id: data._id,
@@ -307,17 +235,23 @@ export class MarkersService {
       height: 20,
       source: data,
       iconSelected,
-      icon
+      icon,
     };
   }
 
-  private regionToExtent(data: MapRegion) {
+  /** 转换为搜索结果 marker */
+  convertToLocationMarker(data: MarkerData): Marker {
     return {
-      xmax: data.northeast.latitude, // 地图区域的上边界
-      ymax: data.northeast.longitude, // 地图区域的右边界
-      xmin: data.southwest.latitude, // 地图区域的下边界
-      ymin: data.southwest.longitude // 地图区域的左边界
-    }
+      id: this.markIdCount++,
+      _id: data._id,
+      latitude: data.x,
+      longitude: data.y,
+      anchor: { x: 0.5, y: 1 },
+      iconPath: "../../assets/location.png",
+      width: 40,
+      height: 40,
+      source: data,
+    };
   }
 }
 
